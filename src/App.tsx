33312958febb145613fb0a0e75f3592194565c37
Иван Глебов <glebov.vanya8@gmail.com>
2022-11-30 23:20:47 +0300

    App component update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo } from "react";
+import "./App.css";
+import data from "./api.json";
+import Table from "./components/Table/Table";
+import { UserType } from "./types";
+import { useSearchParams } from "react-router-dom";
+import SearchBar from "./components/SearchBar/SearchBar";
+import Pagination from "./components/Pagination/Pagination";
 
 function App() {
+  const [params] = useSearchParams();
+
+
+  // Initial entities calculation
+  const entities = useMemo(() => {
+    const res: Array<UserType> = [];
+    data.results.forEach((user) =>
+      res.push({
+        gender: user.gender,
+        country: user.location.country,
+        dob: user.dob.date,
+        email: user.email,
+        phone: user.phone,
+        picture: user.picture.medium,
+        name: `${user.name.title} ${user.name.first} ${user.name.last}`,
+      })
+    );
+    return res;
+  }, [data]);
+
+  // Entities filtering based on search string from query
+  const filteredEntities = useMemo(() => {
+    const search = params.get("search") !== null ? params.get("search") as string : "";
+    return entities.filter((entity) => {
+      return Object.values(entity).some((value) =>
+        value.toLocaleLowerCase().includes(search.toLowerCase())
+      );
+    });
+  }, [params.get("search")]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar />
+      <Table entities={filteredEntities} propsPageSize={9} />
+      <Pagination maxEntites={filteredEntities.length} pageSize={9}/>
     </div>
   );
 }
